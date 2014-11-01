c29a7ae91a666e6cea57e1726431fd2a22d8d999
Vladimir Surov <vovkasu@gmail.com>
2014-11-02 01:45:09 +0200

    Added Java Decompiler package

---
+++ javadecompiler-gui/src/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿
+$version = '0.3.6';
+$packageName = 'javadecompiler-gui'; # arbitrary name for the package, used in messages
+$url = 'http://jd.benow.ca/jd-gui/downloads/jd-gui-${version}.windows.zip'; # download url
+$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
+
+try { 
+
+  Install-ChocolateyZipPackage $packageName $url $unzipLocation;
+
+  Install-ChocolateyShortcut -shortcutFilePath "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::StartMenu))\Programs\JavaDecompiler.lnk" -targetPath "${unzipLocation}\jd-gui.exe";
+  # This will create a new shortcut at the location of "C:\test.lnk" and link to the file located at "C:\text.exe"
+
+  Write-ChocolateySuccess "$packageName";
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)";
+  throw;
+}
