1692f268dbb8c7d41ba673e55cf004ff4b17f6e9
Vladimir Surov <vovkasu@gmail.com>
2014-11-02 02:30:46 +0200

    Fixed install script.

--- javadecompiler-gui/src/tools/chocolateyInstall.ps1
+++ javadecompiler-gui/src/tools/chocolateyInstall.ps1
@@ -1,14 +1,21 @@
 ﻿
 $version = '0.3.6';
 $packageName = 'javadecompiler-gui'; # arbitrary name for the package, used in messages
-$url = 'http://jd.benow.ca/jd-gui/downloads/jd-gui-${version}.windows.zip'; # download url
+$url = "http://jd.benow.ca/jd-gui/downloads/jd-gui-${version}.windows.zip"; # download url
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
+$shortcutFilePath = "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::StartMenu))\Programs\JavaDecompiler.lnk";
+$targetPath = "${unzipLocation}\jd-gui.exe";
 
 try { 
 
   Install-ChocolateyZipPackage $packageName $url $unzipLocation;
 
-  Install-ChocolateyShortcut -shortcutFilePath "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::StartMenu))\Programs\JavaDecompiler.lnk" -targetPath "${unzipLocation}\jd-gui.exe";
+  $global:WshShell = New-Object -com "WScript.Shell"
+  $lnk = $global:WshShell.CreateShortcut($shortcutFilePath)
+  $lnk.TargetPath = $targetPath
+  $lnk.Save()
+
+  #Install-ChocolateyShortcut -shortcutFilePath "$([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::StartMenu))\Programs\JavaDecompiler.lnk" -targetPath "${unzipLocation}\jd-gui.exe";
   # This will create a new shortcut at the location of "C:\test.lnk" and link to the file located at "C:\text.exe"
 
   Write-ChocolateySuccess "$packageName";
