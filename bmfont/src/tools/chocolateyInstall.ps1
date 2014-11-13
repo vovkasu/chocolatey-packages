ae3e25df71925c1846e437679e2da5f28e22ea54
Vladimir Surov <vovkasu@gmail.com>
2014-11-14 00:45:48 +0200

    Added installer for bmfont

---
+++ bmfont/src/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'bmfont'
+$installerType = 'exe' 
+$url = 'http://www.angelcode.com/products/bmfont/install_bmfont_1.13.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
