e8e21a8d5d7011946de4e26738c127b96e46b32d
Vladimir Surov <vovkasu@gmail.com>
2014-11-15 02:44:58 +0200

    Added installer for AIMP

---
+++ aimp/src/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'aimp';
+$installerType = 'exe';
+$url = 'http://files.programki.net/aimp/aimp_3.55.1355.exe';
+$validExitCodes = @(0);
+
+$targetPath = $env:ProgramFiles;
+if (Get-ProcessorBits 64) {
+  $targetPath = ${env:ProgramFiles(x86)};
+}
+
+$silentArgs = "/SILENT /AUTO='$targetPath\AIMP3'";
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
---
+++ aimp/src/tools/chocolateyUninstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'aimp';
+$installerType = 'exe';
+$validExitCodes = @(0);
+
+try {
+	$file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\AIMP3").UninstallString;
+	Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -validExitCodes $validExitCodes -File $file;
+  
+	Write-ChocolateySuccess $packageName;
+} catch {
+	Write-ChocolateyFailure $packageName "$($_.Exception.Message)";
+	throw;
+}
