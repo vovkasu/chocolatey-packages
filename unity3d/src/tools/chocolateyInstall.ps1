449b1d4557504562e07e28ca2d281cff73be6d93
Vladimir Surov <vovkasu@gmail.com>
2014-11-16 02:46:05 +0200

    Added installer for Unity3D

---
+++ unity3d/src/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$version = "4.5.5";
+$packageName = 'unity3d';
+$installerType = 'exe';
+$url = "http://netstorage.unity3d.com/unity/UnitySetup-$version.exe";
+$silentArgs = '/S';
+$validExitCodes = @(0);
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes;
---
+++ unity3d/src/tools/chocolateyUninstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName = 'unity3d';
+$installerType = 'exe';
+$silentArgs = '/S';
+
+try {
+	$file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Unity").UninstallString;
+	Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -silentArgs $silentArgs -File $file;
+  
+	Write-ChocolateySuccess $packageName;
+} catch {
+	Write-ChocolateyFailure $packageName "$($_.Exception.Message)";
+	throw;
+}
