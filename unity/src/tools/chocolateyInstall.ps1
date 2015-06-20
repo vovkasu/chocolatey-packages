6a024d69a1604526672556d4a3f5c1af12ed443a
Vladimir Surov <vovkasu@gmail.com>
2015-06-20 22:17:01 +0300

    Updated Unity3D

--- unity/src/tools/chocolateyInstall.ps1
+++ unity/src/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-﻿$version = "5.0.1";
-$buildName = "5a2e8fe35a68";
+﻿$version = "5.1.1";
+$buildName = "2046fc06d4d8";
 $packageName = 'unity';
 $installerType = 'exe';
-$url = "http://netstorage.unity3d.com/unity/$buildName/Windows32EditorInstaller/UnitySetup32.exe";
-$url64bit = "http://netstorage.unity3d.com/unity/$buildName/Windows64EditorInstaller/UnitySetup64.exe";
+$url = "http://netstorage.unity3d.com/unity/$buildName/Windows32EditorInstaller/UnitySetup32-5.1.1f1.exe";
+$url64bit = "http://netstorage.unity3d.com/unity/$buildName/Windows64EditorInstaller/UnitySetup64-5.1.1f1.exe";
 $silentArgs = '/S';
 $validExitCodes = @(0);
 
