ea2d8fa1c3d72fc52deb659d3bdd9fad7e655afb
Vladimir Surov <vovkasu@gmail.com>
2015-09-17 23:27:23 +0300

    Unity5.2
    Updated Unity3D pack

--- unity/src/tools/chocolateyInstall.ps1
+++ unity/src/tools/chocolateyInstall.ps1
@@ -1,9 +1,10 @@
-﻿$version = "5.1.1";
-$buildName = "2046fc06d4d8";
+﻿$version = "5.2.0";
+$versionName = "5.2.0f3";
+$buildName = "e7947df39b5c";
 $packageName = 'unity';
 $installerType = 'exe';
-$url = "http://netstorage.unity3d.com/unity/$buildName/Windows32EditorInstaller/UnitySetup32-5.1.1f1.exe";
-$url64bit = "http://netstorage.unity3d.com/unity/$buildName/Windows64EditorInstaller/UnitySetup64-5.1.1f1.exe";
+$url = "http://netstorage.unity3d.com/unity/{0}/Windows64EditorInstaller/UnitySetup64-{1}.exe" -f $buildName,$versionName;
+$url64bit = "http://netstorage.unity3d.com/unity/{0}/Windows32EditorInstaller/UnitySetup32-{1}.exe" -f $buildName,$versionName;
 $silentArgs = '/S';
 $validExitCodes = @(0);
 
