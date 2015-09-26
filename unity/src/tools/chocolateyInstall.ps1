f8140b0715320b0c6fe3267f9d6856c21c3562f1
Vladimir Surov <vovkasu@gmail.com>
2015-09-27 00:32:33 +0300

    Unity
    Fixed unity urls

--- unity/src/tools/chocolateyInstall.ps1
+++ unity/src/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $versionName = "5.2.0f3";
 $buildName = "e7947df39b5c";
 $packageName = 'unity';
 $installerType = 'exe';
-$url = "http://netstorage.unity3d.com/unity/{0}/Windows64EditorInstaller/UnitySetup64-{1}.exe" -f $buildName,$versionName;
-$url64bit = "http://netstorage.unity3d.com/unity/{0}/Windows32EditorInstaller/UnitySetup32-{1}.exe" -f $buildName,$versionName;
+$url = "http://netstorage.unity3d.com/unity/{0}/Windows32EditorInstaller/UnitySetup32-{1}.exe" -f $buildName,$versionName;
+$url64bit = "http://netstorage.unity3d.com/unity/{0}/Windows64EditorInstaller/UnitySetup64-{1}.exe" -f $buildName,$versionName;
 $silentArgs = '/S';
 $validExitCodes = @(0);
 
