3af7326b121742ae726a7711b40a18fca718d321
Vladimir Surov <vovkasu@gmail.com>
2015-12-23 23:54:03 +0200

    Unity3D
    updated Unity3D package

--- unity/src/tools/chocolateyInstall.ps1
+++ unity/src/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-﻿$version = "5.2.0";
-$versionName = "5.2.0f3";
-$buildName = "e7947df39b5c";
+﻿$version = "5.3.1";
+$versionName = "5.3.1f1";
+$buildName = "cc9cbbcc37b4";
 $packageName = 'unity';
 $installerType = 'exe';
-$url = "http://netstorage.unity3d.com/unity/{0}/Windows32EditorInstaller/UnitySetup32-{1}.exe" -f $buildName,$versionName;
+$url =      "http://netstorage.unity3d.com/unity/{0}/Windows32EditorInstaller/UnitySetup32-{1}.exe" -f $buildName,$versionName;
 $url64bit = "http://netstorage.unity3d.com/unity/{0}/Windows64EditorInstaller/UnitySetup64-{1}.exe" -f $buildName,$versionName;
 $silentArgs = '/S';
 $validExitCodes = @(0);
