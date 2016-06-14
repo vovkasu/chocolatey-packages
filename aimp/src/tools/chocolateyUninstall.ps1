6d0f514c640c2810c5dd3f27293d284e03d855d0
massiliattak <massiliattak@gmail.com>
2016-06-14 13:42:06 +0200

    Update chocolateyUninstall.ps1
    - HLKM keys modified (uninstall keys name have been changed)
    - Replaced "Write-ChocolateyFailure" (deprecated)

--- aimp/src/tools/chocolateyUninstall.ps1
+++ aimp/src/tools/chocolateyUninstall.ps1
@@ -1,17 +1,16 @@
-﻿$packageName = 'aimp';
+$packageName = 'aimp';
 $installerType = 'exe';
 $validExitCodes = @(0);
 
 try {
-	$hklm = "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\AIMP3";
+	$hklm = "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\AIMP";
 	if (Get-ProcessorBits 64) {
-  		$hklm = "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\AIMP3";
+  		$hklm = "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\AIMP";
 	}
 	$file = (Get-ItemProperty -Path $hklm).UninstallString;
 	Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -validExitCodes $validExitCodes -File $file;
   
 	Write-ChocolateySuccess $packageName;
 } catch {
-	Write-ChocolateyFailure $packageName "$($_.Exception.Message)";
-	throw;
+throw $_.Exception
 }
