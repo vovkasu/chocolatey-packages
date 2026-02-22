88922a6a670af77a2389fcfc9a3595b5760c2947
Surov Vladimir <vovkasu@gmail.com>
2026-02-22 18:57:25 +0200

    Update AIMP to 5.40.2703 and installer metadata

--- aimp/tools/chocolateyInstall.ps1
+++ aimp/tools/chocolateyInstall.ps1
@@ -1,21 +1,32 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'aimp';
-$url32 = 'https://aimp.ru/files/windows/builds/aimp_5.02.2370.exe';
+$url32 = 'https://aimp.ru/files/windows/builds/aimp_5.40.2703_w32.exe';
           
-$checksum32  = ''
+$checksum32 = '50c3aedab55aa7263284d9761868e344e7f174aff205f5af95e30548dab9ca30';
+
+$url64 = 'https://aimp.ru/files/windows/builds/aimp_5.40.2703_w64.exe';
+          
+$checksum64 = '080981925a962749e2b99b10b6a199204ef4fc9d5b836dca7415db96e77ef8ef';
+
 
 $silentArgs = "/SILENT /AUTO";
 
 
 $packageArgs = @{
-  packageName            = $packageName
-  fileType               = 'EXE'
-  url                    = $url32
-  checksum               = $checksum32
-  checksumType           = 'sha256'
-  silentArgs             = $silentArgs
-  validExitCodes         = @(0)
+  PackageName            = $packageName
+  FileType               = 'EXE'
+
+  Url                    = $url32
+  Checksum               = $checksum32
+  ChecksumType           = 'sha256'
+
+  Url64bit               = $url64
+  Checksum64             = $checksum64
+  ChecksumType64         = 'sha256'
+
+  SilentArgs             = $silentArgs
+  ValidExitCodes         = @(0)
   registryUninstallerKey = $packageName
 }
 
--- aimp/tools/chocolateyUninstall.ps1
+++ aimp/tools/chocolateyUninstall.ps1
@@ -2,15 +2,9 @@ $packageName = 'aimp';
 $installerType = 'exe';
 $validExitCodes = @(0);
 
-try {
-	$hklm = "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\AIMP";
-	if (Get-ProcessorBits 64) {
-  		$hklm = "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\AIMP";
-	}
-	$file = (Get-ItemProperty -Path $hklm).UninstallString;
-	Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -validExitCodes $validExitCodes -File $file;
-  
-	Write-ChocolateySuccess $packageName;
-} catch {
-throw $_.Exception
+$hklm = "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\AIMP";
+if (Get-ProcessorBits 64) {
+	$hklm = "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\AIMP";
 }
+$file = (Get-ItemProperty -Path $hklm).UninstallString;
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -validExitCodes $validExitCodes -File $file;
